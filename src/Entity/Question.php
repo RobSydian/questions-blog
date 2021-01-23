8dee483f9df714e7da2bb04b6de4f9639d991cbb
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2021-01-23 11:46:53 +0100

    Final step, make question slugs automatic

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -5,15 +5,18 @@ namespace App\Entity;
 use App\Repository\QuestionRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass=QuestionRepository::class)
  */
 class Question
 {
+    use TimestampableEntity;
+
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
